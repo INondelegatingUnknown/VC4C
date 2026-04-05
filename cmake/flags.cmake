40019b9efa3df6b25ae332389dd442e4ee768970
INondelegatingUnknown <curly69rail@gmail.com>
2026-04-06 02:36:07 +0300

    Add/remove compiler flags to minimize non-critical warnings.

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -3,18 +3,23 @@ set(VC4C_ENABLED_WARNINGS
 	-Wold-style-cast -Wnon-virtual-dtor -Wnull-dereference
 	-Wno-unused-parameter -Wno-missing-field-initializers -Wno-write-strings -Wno-float-equal
 	-Werror=return-type -Werror=unused-result -Werror=shift-count-overflow -Werror=missing-field-initializers -Werror=reorder
+	-Wunknown-warning-option
+	-Wno-macro-redefined -Wno-deprecated-declarations
+	-Wno-switch-default
+	-Wno-unsafe-buffer-usage
 )
 # Enable additional warnings, if available
 if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
 	SET(VC4C_ENABLED_WARNINGS
 		${VC4C_ENABLED_WARNINGS}
 		-Weverything
-		-Wswitch
+		#-Wswitch
 		-Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-shadow -Wno-padded -Wno-shadow-field-in-constructor -Wno-global-constructors
 		-Wno-exit-time-destructors -Wno-missing-prototypes -Wno-gnu-anonymous-struct -Wno-nested-anon-types -Wno-documentation
 		-Wno-unused-command-line-argument -Wno-unused-member-function -Wno-gnu-zero-variadic-macro-arguments -Wno-covered-switch-default
-		-Wno-switch-enum -Wno-return-std-move-in-c++11 -Wno-shadow-field -Wno-suggest-destructor-override
+		-Wno-switch-enum -Wno-shadow-field -Wno-suggest-destructor-override
 		-Werror=return-stack-address
+		-Wno-unsafe-buffer-usage
 	)
 elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
 	SET(VC4C_ENABLED_WARNINGS
@@ -24,6 +29,7 @@ elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
 		-Wuninitialized -Wsuggest-attribute=format -Wsuggest-override -Wconversion -Wzero-as-null-pointer-constant
 		-Wno-psabi -Wno-unknown-pragmas
 		-Werror=return-local-addr -Werror=uninitialized
+		-Wno-return-std-move-in-c++11
 	)
 endif()
 
