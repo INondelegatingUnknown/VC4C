9480c626f5a4d91eb5a303ee5f58d58b2298f513
INondelegatingUnknown <curly69rail@gmail.com>
2026-04-06 16:53:22 +0300

    Remove unreachable code.

--- src/analysis/ControlFlowLoop.cpp
+++ src/analysis/ControlFlowLoop.cpp
@@ -81,9 +81,8 @@ Optional<Literal> InductionVariable::getStep() const
 
     if(auto stepWriter = otherArgument->getSingleWriter())
         return stepWriter->precalculate(4).first & &Value::getLiteralValue;
-    return otherArgument->getConstantValue() & &Value::getLiteralValue;
 
-    return {};
+    return otherArgument->getConstantValue() & &Value::getLiteralValue;
 }
 
 Optional<ValueRange> InductionVariable::getRange() const
